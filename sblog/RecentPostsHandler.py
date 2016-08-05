8f4665107fd300ceb67369e0f049972067346188
developer2020 <dev276236@gmail.com>
2016-08-05 17:16:15 +0400

    refactored like/dislike functionality into one common function

--- sblog/RecentPostsHandler.py
+++ sblog/RecentPostsHandler.py
@@ -4,7 +4,6 @@ from BlogData import BlogData
 import HashLib
 
 
-
 class RecentPostsHandler(Handler):
     current_posts = None
 
@@ -17,6 +16,22 @@ class RecentPostsHandler(Handler):
         self.render("recent_posts.html", posts=posts, misc_data=str(""))
         RecentPostsHandler.current_posts = None
 
+    def update_likes(self, posts, error_message, post_to_update_idx_string, user_name, likes_counter_function):
+
+        if post_to_update_idx_string.isdigit():
+            post_idx = int(post_to_update_idx_string)
+            post = BlogData.get_post_by_id(post_idx)
+            if post.user_name == user_name:
+                posts[BlogData.get_post_idx_in_a_list(posts, post_idx)].error_message = error_message
+                RecentPostsHandler.current_posts = posts
+            else:
+                post.number_of_likes = likes_counter_function(post.number_of_likes)
+                posts[BlogData.get_post_idx_in_a_list(posts, post_idx)] = post
+                post.put()
+            return True
+
+        return False
+
     def post(self):
         user_name = HashLib.check_user_name_cookie(self, "/login")
         if user_name is None:
@@ -24,34 +39,18 @@ class RecentPostsHandler(Handler):
 
         liked_post_idx_string = self.request.get("liked_post_idx").strip()
         unliked_post_idx_string = self.request.get("unliked_post_idx").strip()
-
         posts = list(db.GqlQuery("SELECT * FROM Post ORDER BY created DESC LIMIT 10"))
+        self.update_likes(posts,  "Error: cannot like your own posts",
+                          liked_post_idx_string, user_name, lambda x: x + 1)
 
-        if liked_post_idx_string.isdigit():
-            liked_post_idx = int(liked_post_idx_string)
-            liked_post = BlogData.get_post_by_id(liked_post_idx)
-            if liked_post.user_name == user_name:
-                posts[BlogData.get_post_idx_in_a_list(posts, liked_post_idx)].error_message = "Error: cannot like your own posts"
-            else:
-                liked_post.number_of_likes += 1
-                posts[BlogData.get_post_idx_in_a_list(posts, liked_post_idx)] = liked_post
-                liked_post.put()
+        self.update_likes(posts,  "Error: cannot unlike your own posts",
+                          unliked_post_idx_string, user_name, lambda x: x - 1)
 
-        elif unliked_post_idx_string.isdigit():
-            unliked_post_idx = int(unliked_post_idx_string)
-            unliked_post = BlogData.get_post_by_id(unliked_post_idx)
-            if unliked_post.user_name == user_name:
-                posts[BlogData.get_post_idx_in_a_list(posts, unliked_post_idx)].error_message = "Error: cannot unlike your own posts"
-
-            else:
-                unliked_post.number_of_likes -= 1
-                posts[BlogData.get_post_idx_in_a_list(posts, unliked_post_idx)] = unliked_post
-                unliked_post.put()
-
-        RecentPostsHandler.current_posts = posts
         #need to rediect to get to avoid re-submission of post request - we want error message to clear
         #if user refreshes the page
         self.redirect("/recentposts")
+
+
 
 
 
