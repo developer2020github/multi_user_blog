1dc85ee09fade301b737592f072c69b74af3b681
developer2020 <dev276236@gmail.com>
2016-08-05 13:48:48 +0400

    added unlike button to posts

--- sblog/RecentPostsHandler.py
+++ sblog/RecentPostsHandler.py
@@ -9,10 +9,20 @@ class RecentPostsHandler(Handler):
         self.render("recent_posts.html", posts=posts)
 
     def post(self):
-        liked_post_idx = int(self.request.get("liked_post_idx"))
-        liked_post = BlogData.get_post_by_id(liked_post_idx)
-        liked_post.number_of_likes += 1
-        liked_post.put()
+        liked_post_idx_string = self.request.get("liked_post_idx").strip()
+        unliked_post_idx_string = self.request.get("unliked_post_idx").strip()
+
+        if liked_post_idx_string.isdigit():
+            liked_post_idx = int(liked_post_idx_string)
+            liked_post = BlogData.get_post_by_id(liked_post_idx)
+            liked_post.number_of_likes += 1
+            liked_post.put()
+        elif unliked_post_idx_string.isdigit():
+            unliked_post_idx = int(unliked_post_idx_string)
+            unliked_post = BlogData.get_post_by_id(unliked_post_idx)
+            unliked_post.number_of_likes -= 1
+            unliked_post.put()
+
         posts = db.GqlQuery("SELECT * FROM Post ORDER BY created DESC LIMIT 10")
         self.render("recent_posts.html", posts=posts)
 
