3a054e92ca4e9f80a9dbd2900ae93d9a77eeaaae
developer2020 <dev276236@gmail.com>
2016-08-19 19:25:57 +0400

    Ran current post handler though pylint and updated it accordingly.

--- sblog/CurrentPostHandler.py
+++ sblog/CurrentPostHandler.py
@@ -1,9 +1,23 @@
+"""This module contains a handler
+for current post page
+(displays one posts with all teh comments)
+"""
+
 from Handler import Handler
 from BlogData import BlogData
 
 
 class CurrentPostHandler(Handler):
+    """
+    Current post handler - renders current post page
+    - a post with comments
+    """
     def get(self, post_id):
+        """
+        Processes get request and renders the page
+        :param post_id: ID of the post to render
+        :return: None
+        """
         post = BlogData.get_post_by_id(post_id)
         comments = BlogData.get_comments_by_post_id(post_id)
         comments_title = ""
