2186e512e5036f15688ef4df4a7f1ebcbe984798
developer2020 <dev276236@gmail.com>
2016-08-20 12:56:20 +0400

    ran login handler through PyLint and updated it accordingly

--- sblog/LogInHandler.py
+++ sblog/LogInHandler.py
@@ -1,76 +1,45 @@
+"""This module contains a handler
+for login page
+"""
 from Handler import Handler
-'''
-storing passwords:
-table of user->hash(pw +salt), salt values, where salt is some random characters
-
-look for function make_salt in homewrok
-    def make_salt():
-    ###Your code here
-    return ''.join(random.choice(string.ascii_letters) for n in range(5))
-
-def make_salt():
-    return ''.join(random.choice(string.letters) for x in xrange(5))
-
-# implement the function make_pw_hash(name, pw) that returns a hashed password
-# of the format:
-# HASH(name + pw + salt),salt
-# use sha256
-
-def make_pw_hash(name, pw):
-    ###Your code here
-    salt = make_salt()
-    h=hashlib.sha256(name+pw+salt).hexdigest()
-    return "%s,%s" % (h, salt)
-
-=================================================
-vaidating salts
-
-def make_salt():
-    return ''.join(random.choice(string.letters) for x in xrange(5))
-
-# Implement the function valid_pw() that returns True if a user's password
-# matches its hash. You will need to modify make_pw_hash.
-
-def make_pw_hash(name, pw, salt=""):
-    if salt=="":
-        salt = make_salt()
-
-    h = hashlib.sha256(name + pw + salt).hexdigest()
-    return '%s,%s' % (h, salt)
-
-def valid_pw(name, pw, h):
-    ###Your code here
-    salt = h.split(",")[1]
-    if h ==make_pw_hash(name, pw, salt):
-        return True
-
-    return False
-
-h = make_pw_hash('spez', 'hunter2')
-print valid_pw('spez', 'hunter2', h)
-
-try using bcrypt
-===================
-'''
 from BlogData import BlogData
 import HashLib
 
 
 class LogInHandler(Handler):
+    """
+    Login page handler class.
+    """
 
     def get(self):
+        """
+        Rendered login page. No checks performed.
+        :return: None
+        """
         self.render("login.html")
 
     def post(self):
+        """
+        Checks submitted user name and password.
+        If user name and password are valid -
+        adds username cookie and
+        redirects to Welcome page.
+        Otherwise, re-renders page with corresponding
+        error message
+        :return: None
+        """
         user_name = self.request.get("username")
         password = self.request.get("password")
 
         if BlogData.user_exists(user_name) is None:
-            self.render("login.html", username_error_message="Error: user name " + user_name + " does not exist")
+            self.render("login.html", username_error_message=
+                        "Error: user name " + user_name + " does not exist")
             return
 
         if BlogData.user_password_ok(user_name, password):
-            self.response.headers.add_header("Set-Cookie", "user_id=%s" % str(HashLib.make_secure_cookie(user_name)))
+            self.response.headers.add_header("Set-Cookie",
+                                             "user_id=%s" %
+                                             str(HashLib.make_secure_cookie(user_name)))
             self.redirect("/welcome")
         else:
             self.render("login.html", password_error_message="Error: wrong password")
