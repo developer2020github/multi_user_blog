6b263cc54529eedce9190cda82d2c8f5ab76dc32
developer2020 <dev276236@gmail.com>
2016-08-20 13:46:09 +0400

    ran log out hanlder throught PyLint and updated it accordingly

--- sblog/LogOutHandler.py
+++ sblog/LogOutHandler.py
@@ -1,7 +1,20 @@
+"""This module contains a handler
+for logout page
+"""
+
 from Handler import Handler
 
 
 class LogOutHandler(Handler):
+    """
+    Log out handler class
+    """
     def get(self):
+        """
+        There is not special log out page,
+        so handler just deletes user name
+        cookie and re-directs to sign up page
+        :return: None
+        """
         self.response.headers.add_header("Set-Cookie", "user_id=; Path=/")
         self.redirect("/signup")
