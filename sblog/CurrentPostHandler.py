024b9fdbc3462c0a5f5012214a8b861290f2e3ba
developer2020 <dev276236@gmail.com>
2016-08-21 16:51:20 +0400

    updated comments in currentpost handler

--- sblog/CurrentPostHandler.py
+++ sblog/CurrentPostHandler.py
@@ -14,6 +14,7 @@ class CurrentPostHandler(Handler):
     - a post with comments
     """
     current_post = None
+
     def get(self, post_id):
         """
         Processes get request and renders the page
@@ -26,10 +27,11 @@ class CurrentPostHandler(Handler):
             post = BlogData.get_post_by_id(post_id)
 
         comments = BlogData.get_comments_by_post_id(post_id)
-        comments_title = ""
 
+        comments_title = ""
         if post.number_of_comments > 0:
             comments_title = "Comments:"
+
         CurrentPostHandler.current_post = None
         self.render("current_post.html",
                     post=post, comments=comments,
@@ -73,14 +75,13 @@ class CurrentPostHandler(Handler):
         """
         Processes submitted by user like or dislike button click.
         Checks if user is trying to like/dislike his own post
-        and sets error message for corresponding post in the list.
-         Updated likes counter and list of users that liked (or disliked)
-         the post.
-        :param posts: list of currently displayed posts
+        and sets error message for teh post.
+        Updates likes counter and list of users that liked (or disliked)
+        the post.
         :param error_message: message to display in case there is an error
                               (user is tyring to like or dislike his own post)
-        :param post_to_update_idx_string: post which user is trying to like
-                                          or dislike
+        :param post_to_update_idx_string: post id which user is trying to like
+                                          or dislike (comes from post request)
         :param user_name: user name
         :param likes_counter_function: a simple lambda function that updates likes counter:
                                        update_likes is common between likes
@@ -95,6 +96,8 @@ class CurrentPostHandler(Handler):
             if post:
                 if post.user_name == user_name:
                     post.error_message = error_message
+                    # error message does not need to be saved in the database, so
+                    # pass it through class member variable
                     CurrentPostHandler.current_post = post
                 elif BlogData.post_was_liked_by_user(user_name, post):
                     return False
